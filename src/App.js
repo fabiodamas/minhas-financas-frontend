cfb733f983cc1b86c2146ecf33ce35f71c6d1132
Fabio Damas <fabio.damas@gmail.com>
2021-07-21 14:44:56 -0300

    customizando o login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "bootswatch/dist/flatly/bootstrap.css";
-import Login from './views/login'
+import Login from './views/login';
+import "./custom.css";
+
 
 class App extends React.Component {
 
